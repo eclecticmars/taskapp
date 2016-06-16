44dcabbebae06ddf2d5dc7f433d0a9369792c0e7
makoto.shirai <makotoshirai@gmail.com>
2016-06-16 21:21:53 +0900

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'XcRSy6uMPUKlVgx6n3fkxNDr7', 'THwkFmOoii1AzZNxsIS4tPCOdOgoCvtyiWPRknqus3WjQlONui'
   provider :facebook, 'APP_ID', 'APP_SECRET'
   provider :Linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  //comment
 end
